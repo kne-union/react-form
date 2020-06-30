0286adf5bedba8f5100abdae160d311b73722f59
林志鹏 <zhipenglin@knx.com.cn>
2020-06-30 17:53:41 +0800

    修改组件值获取逻辑

--- src/util/getFieldValue.js
+++ src/util/getFieldValue.js
@@ -3,7 +3,14 @@ import isEvent from './isEvent';
 export default (event, value) => {
   if (isEvent(event)) {
     if (value === undefined) {
-      value = event.target.value;
+      switch (event.target.type) {
+        case 'checkbox':
+        case 'radio':
+          value = event.target.checked;
+          break;
+        default:
+          value = event.target.value;
+      }
     }
   } else {
     value = event;
